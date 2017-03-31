21448637e85e7fe6b6a00fe3d136cbca70821db2
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-03-31 17:50:53 +0200

    Remove test result viewer link
    We now add it directly on github, so it'll show up in the normal status checks

--- monors.py
+++ monors.py
@@ -371,16 +371,6 @@ class PullReq:
           att["mrkdwn_in"] = ["text"]
           attachments.append (att)
 
-        # add PR result viewer link
-        prviewer_link = "https://jenkins.mono-project.com/view/All/job/jenkins-testresult-viewer/Test_Result_View/builds.html#groupBy=PRs&span=Last7Days&filterPr=%s" % self.num
-        att = {}
-        att["fallback"] = "PR result viewer: %s" % prviewer_link
-        att["title"] = "<%s|PR result viewer>" % prviewer_link
-        att["text"] = "Shows aggregated test results and analyzes failure causes."
-        att["color"] = "#d3d3d3"
-        att["mrkdwn_in"] = ["text"]
-        attachments.append (att)
-
         if not self.id in history:
           history[self.id] = {}
 
